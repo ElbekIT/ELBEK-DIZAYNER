4f065709cb683fc1dd98e8b2250f266d6c529a8f
ElbekIT <qoriyevagavharoy@gmail.com>
2026-02-07 11:13:07 +0500

    add

--- firebase.ts
+++ firebase.ts
@@ -1,20 +1,25 @@
-
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "firebase/auth";
-import { getDatabase, ref, set, push, onValue, get } from "firebase/database";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut
+} from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAGSvG6gqUz198-Y7NMLKq8dnYRmLPE7-o",
   authDomain: "darian-electronics.firebaseapp.com",
   databaseURL: "https://darian-electronics-default-rtdb.firebaseio.com",
   projectId: "darian-electronics",
-  storageBucket: "darian-electronics.firebasestorage.app",
+  storageBucket: "darian-electronics.appspot.com", // ✅ TO‘G‘RI
   messagingSenderId: "439635255126",
   appId: "1:439635255126:web:e666b5bde36eaaf3ac40f7",
   measurementId: "G-LXS4VHCQZ7"
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const database = getDatabase(app);
 export const googleProvider = new GoogleAuthProvider();
